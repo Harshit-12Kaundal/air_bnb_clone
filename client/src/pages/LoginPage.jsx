0afda4cf37af3bfee22890b58057589c95b22f42
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-07-10 23:25:08 +0530

    login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,21 +1,32 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
+import {Link,Navigate} from 'react-router-dom'
 import axios from 'axios';
+import { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
 export default function LoginPage(){
 
     const[email,setEmail]=useState('');
     const[password,setPassword]=useState('');
+    const[redirect,setRedirect]=useState(false);
+
+    const {setUser}=useContext(UserContext);
 
     async function handleLoginSubmit(ev){
         ev.preventDefault();
         try{
-            await axios.post('/login',{email,password},{withCredentials:true });
+            const {data}=await axios.post('/login',{email,password}, {withCredentials:true });
+            setUser(data);
             alert('Login sucessful');
+            setRedirect(true);
         }
         catch(e){
             alert('Login Failed');
         }
+    }
+    
+    if(redirect){
+        return <Navigate to={'/'}/>
     }
 
     return(
